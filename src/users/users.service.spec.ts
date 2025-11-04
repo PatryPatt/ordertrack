7b645d662e13c047dff3dc54b873698deb7fa316
Patricia <patricia.pascual@alten.es>
2025-11-04 21:50:36 +0100

    initial commit NestJS backend

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,43 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [];
+  private idCounter = 1;
+
+  create(createUserDto: CreateUserDto): User {
+    const newUser: User = {
+      id: this.idCounter++,
+      ...createUserDto,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  findOne(id: number): User {
+    const user = this.users.find((u) => u.id === id);
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return user;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto): User {
+    const user = this.findOne(id);
+    const updatedUser = { ...user, ...updateUserDto };
+    this.users = this.users.map((u) => (u.id === id ? updatedUser : u));
+    return updatedUser;
+  }
+
+  remove(id: number): void {
+    const user = this.findOne(id);
+    this.users = this.users.filter((u) => u.id !== user.id);
+  }
+}
