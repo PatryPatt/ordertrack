670285e9698e70c71c8b2c650829302d1f1d522a
Patricia <patricia.pascual@alten.es>
2025-11-05 15:34:10 +0100

    feat(primer endpoint CRUD):Creación del esqueleto del backend con NestJS, estructura modular y primer endpoint CRUD. Desplegar una API mínima funcional y documentada con Swagger.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  // ✅ Habilitar validaciones globales con class-validator
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+
+  // ✅ Configurar Swagger
+  const config = new DocumentBuilder()
+    .setTitle('OrderTrack API')
+    .setDescription('API de gestión de usuarios y pedidos')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
+  console.log(`🚀 App running on http://localhost:3001`);
+  console.log(`📘 Swagger Docs on http://localhost:3001/api`);
 }
-bootstrap();
+void bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
-  name: string;
+  @ApiProperty({ example: 'patricia', description: 'Nombre de usuario' })
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @ApiProperty({
+    example: 'patricia@example.com',
+    description: 'Correo electrónico del usuario',
+  })
+  @IsEmail()
   email: string;
+
+  @ApiProperty({ example: '123456', description: 'Contraseña del usuario' })
+  @IsString()
+  @MinLength(6)
+  password: string;
 }
