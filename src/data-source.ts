fb6aa24824eac5e403bfa35ed8c262d2b6adccda
Patricia <patricia.pascual@alten.es>
2025-11-12 23:39:54 +0100

    feat: tenemos PostgreSQL corriendo en Docker instalamos las dependencias y elegimos trabajar con TypeORM

--- src/data-source.ts
+++ src/data-source.ts
@@ -9,11 +9,12 @@ dotenv.config(); // carga variables de entorno desde .env
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.POSTGRES_HOST || 'postgres',
-  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
-  username: process.env.POSTGRES_USER || 'user',
-  password: process.env.POSTGRES_PASSWORD || 'password',
-  database: process.env.POSTGRES_DB || 'app_db',
+  url: process.env.DATABASE_URL, // <-- usamos DATABASE_URL
+  //host: process.env.POSTGRES_HOST || 'postgres',
+  //port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+  //username: process.env.POSTGRES_USER || 'user',
+  //password: process.env.POSTGRES_PASSWORD || 'password',
+  //database: process.env.POSTGRES_DB || 'app_db',
   entities: [User],
   migrations: ['src/migrations/*.ts'], // para generar migraciones en desarrollo
   //migrations: ['dist/migrations/*.js'], // Aquí irán las migraciones compiladas
